1d54d342126fcf1f39b89986aef89d4b1cf78f72
Melinda Chirila <mchirila980@west-mec.org>
2019-04-26 13:54:18 -0700

    function of the calccart

--- mas_register.js
+++ mas_register.js
@@ -51,3 +51,39 @@ function sessionsTest() {
             sessionBox.setCustomValidity("");
       }
 };
+
+function calcCart() {
+      // Gets the values of the fields, and stores the values within variables, so that they can be uused later. The variables are used later on to update the page with the information.
+      sessionStorage.setItem("confName", document.getElementById("fnBox").value + " " + document.getElementById("lnBox").value + "")
+      sessionStorage.setItem("confGroup", document.getElementById("groupBox").value)
+      sessionStorage.setItem("confMail", document.getElementById("mailBox").value)
+      sessionStorage.setItem("confPhone", document.getElementById("phoneBox").value)
+      sessionStorage.setItem("confBanquet", document.getElementById("banquetBox").value)
+      var confBanquet = document.getElementById("banquetBox").value;
+      sessionStorage.setItem("confBanquetCost", confBanquet * 55)
+      var confBanquetCost = confBanquet * 55;
+      var sessionBox = document.getElementById("sessionBox");
+      // Gets what options are selected from the number of people attending the banquet, and whether the person has purchased a media pack, which is stored in variables to be used later
+      if (sessionBox.selectedIndex !== -1) {
+            sessionStorage.setItem("confSession", sessionBox[sessionBox.selectedIndex].innerText)
+            sessionStorage.setItem("confSessionCost", sessionBox[sessionBox.selectedIndex].value)
+            var confSessionCost = sessionBox[sessionBox.selectedIndex].value;
+      } else {
+            sessionStorage.setItem("confSession", "")
+            sessionStorage.setItem("confSessionCost", 0)
+            var confSessionCost = 0;
+      }
+      if (document.getElementById("mediaCB").checked == true) {
+            sessionStorage.setItem("confPack", "yes")
+            sessionStorage.setItem("confPackCost", 115)
+            var confPackCost = 115;
+      } else {
+            sessionStorage.setItem("confPack", "no")
+            sessionStorage.setItem("confPackCost", 0)
+            var confPackCost = 0;
+      }
+      // Gets the total amount of money
+      sessionStorage.setItem("confTotal", parseFloat(confSessionCost) + parseFloat(confBanquetCost) + parseFloat(confPackCost));
+      //Runs the function that updates the page
+      writeSessionValues();
+};
